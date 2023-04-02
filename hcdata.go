04488771b705910f2abba11db5996271fec5ce1b
kaf <897243839@qq.com>
2023-04-02 14:53:54 +0800

    Initial commit

---
+++ hcdata.go
@@ -0,0 +1,8 @@
+package hotclod
+
+var (
+	block_hot                   = "blockhot.json"
+	compressflag                = "compressflag.json"
+	maphot                      = New[int]()
+	Mode         CompressorType = 1
+)
